c6d5b0652187eb3fe1ecf6638d836afa87bf3c8b
RESOLUM <130007951+RESOLUM@users.noreply.github.com>
2023-05-03 14:47:46 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -42,3 +42,53 @@ const closeNav = () => {
 }
 
 closeBtn.addEventListener('click', closeNav)
+
+
+//Mostrar forms para que envien soluciones
+
+
+
+// Obtener los botones y los divs correspondientes
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var boton1 = document.getElementById("boton1");
+    var informacion1 = document.getElementById("informacion1");
+    var boton2 = document.getElementById("boton2");
+    var informacion2 = document.getElementById("informacion2");
+    var boton3 = document.getElementById("boton3");
+    var informacion3 = document.getElementById("informacion3");
+    var boton4 = document.getElementById("boton4");
+    var informacion4 = document.getElementById("informacion4");
+
+    // Función para mostrar u ocultar la información al hacer click en el botón
+    function toggleInfo(boton, informacion) {
+      if (informacion.style.display === "none") {
+        informacion.style.display = "block";
+        boton.innerHTML = "Ocultar";
+      } else {
+        informacion.style.display = "none";
+        boton.innerHTML = "Leer más";
+      }
+    }
+
+    // Agregar event listeners a los botones
+    boton1.addEventListener("click", function() {
+      toggleInfo(boton1, informacion1);
+    });
+
+    boton2.addEventListener("click", function() {
+      toggleInfo(boton2, informacion2);
+    });
+
+    boton3.addEventListener("click", function() {
+      toggleInfo(boton3, informacion3);
+    });
+
+    boton4.addEventListener("click", function() {
+      toggleInfo(boton4, informacion4);
+    });
+  });
+
+
+
